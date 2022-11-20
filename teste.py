3bf2e87350ad088601d380358fcb454a9398f68d
arthur-miguel <arthur.miguel@grad.ufsc.br>
2022-11-20 20:29:53 -0300

    changed exemple file

--- teste.py
+++ teste.py
@@ -1,15 +1,21 @@
+# Imports ground_track library
 from ground_track import *
 
-iss = Orbit( a = 417 + 6371,
-             e = 0.01,
-             I = 51.64 * (np.pi/180))
+# Molnyia ground track
+molnyia = Orbit( a = 26600, #major semiaxis
+                0.74, #eccentricity
+                63.4 * (np.pi/180), #inclination
+                80 * (np.pi/180), #accending node
+                270 * (np.pi/180) #periapsis argument
+               )
+molnyia.evaluate(np.linspace(0, 2*molnyia.period, 10000)) # evaluates orbit at two periods
+molnyia.save_data("molnyia.txt") # saves text file with results
+molnyia.plot_track() # plots ground track
+
+# Space Station gorund track
+iss = Orbit(417 + 6371, 0.01, 51.64 * (np.pi/180))
 
 iss.evaluate(np.linspace(0, 24*3600, 10000))
-fname = "iss.txt"
-iss.save_data(fname)
-iss.plot_track(fname)
+iss.save_data("iss.txt")
+iss.plot_track()
 
-molnyia = Orbit(26600, 0.74, 63.4 * (np.pi/180), 80 * (np.pi/180), 270 * (np.pi/180))
-molnyia.evaluate(np.linspace(0, 24*3600, 10000))
-molnyia.save_data("molnyia.txt")
-molnyia.plot_track("molnyia.txt")
